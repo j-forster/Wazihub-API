b26a5e05abd85a2480080bbd681e5073175345b1
J Forster <johannforster15@gmail.com>
2018-11-14 17:53:34 +0100

    actuator value fix

--- actuators.go
+++ actuators.go
@@ -1,11 +1,11 @@
 package wazihub
 
 type Actuator struct {
-	Id              string `json:"id"`
-	Name            string `json:"name"`
-	ActuationDevice string `json:"actuating_device"`
-	ControlType     string `json:"control_type"`
-	Value           string `json:"value"`
+	Id              string      `json:"id"`
+	Name            string      `json:"name"`
+	ActuationDevice string      `json:"actuating_device"`
+	ControlType     string      `json:"control_type"`
+	Value           interface{} `json:"value"`
 }
 
 func Actuation(deviceId, actuatorId string) (chan string, error) {
