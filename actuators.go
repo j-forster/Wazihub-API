aa6fd804bbaec3ac3901382b27d9756a75b465bc
J Forster <johannforster15@gmail.com>
2018-11-14 13:00:40 +0100

    api progress

--- actuators.go
+++ actuators.go
@@ -8,7 +8,25 @@ type Actuator struct {
 	Value           string `json:"value"`
 }
 
-func Actuation(deviceId, actuatorId string) chan string {
+func Actuation(deviceId, actuatorId string) (chan string, error) {
 
-	return Subscribe("devices/" + deviceId + "/actuators/" + actuatorId + "/value")
+	if client == nil {
+		panic("Call wazihub.Login() before using this function.")
+	}
+
+	subs, err := client.Subscribe("devices/"+deviceId+"/actuators/"+actuatorId+"/value", 0x00)
+	if err != nil {
+		return nil, err
+	}
+
+	channel := make(chan string)
+
+	go (func() {
+		for msg := range subs {
+			channel <- string(msg.Data)
+		}
+		close(channel)
+	})()
+
+	return channel, nil
 }
--- auth.go
+++ auth.go
@@ -1,1 +1,23 @@
 package wazihub
+
+import (
+	"github.com/j-forster/Wazihub-API/mqtt"
+)
+
+var client *mqtt.Client
+
+func Login(username, password string) error {
+
+	auth := &mqtt.ConnectAuth{
+		Username: username,
+		Password: password,
+	}
+
+	id := CurrentDeviceId()
+
+	c, err := mqtt.Dial(":1883", id, auth, nil)
+	if err == nil {
+		client = c
+	}
+	return err
+}
