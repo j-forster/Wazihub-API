a3a5a0e872c789923dd9d65a3106f29223de0bd8
J Forster <johannforster15@gmail.com>
2018-11-26 15:14:53 +0100

    added 'PostValue'

--- sensors.go
+++ sensors.go
@@ -1,5 +1,13 @@
 package wazihub
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type Sensor struct {
 	Id            string      `json:"id"`
 	Name          string      `json:"name"`
@@ -8,4 +16,26 @@ type Sensor struct {
 	Unit          string      `json:"unit"`
 	LastValue     interface{} `json:"last_value"`
 	Calibration   interface{} `json:"calibration"`
+}
+
+func PostValue(deviceId string, sensorId string, value interface{}) error {
+
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	buf := bytes.NewBuffer(data)
+
+	resp, err := http.Post(Cloud+"devices/"+deviceId+"/sensors/"+sensorId+"/value", "application/json", buf)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("Recieved status %q:%q", resp.Status, body)
+	}
+	return nil
 }
